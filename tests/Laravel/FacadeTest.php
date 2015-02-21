ce93dc8e39b327ad5aa8afd5d5ad1e560d02c548
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-02-21 17:36:13 +0000

    Adding more tests

--- tests/Laravel/FacadeTest.php
+++ tests/Laravel/FacadeTest.php
@@ -1,7 +1,9 @@
 <?php namespace Arcanedev\Breadcrumbs\Tests\Laravel;
 
+use Arcanedev\Breadcrumbs\Builder;
 use Arcanedev\Breadcrumbs\Laravel\Facade as Breadcrumbs;
 use Arcanedev\Breadcrumbs\Tests\LaravelTestCase;
+use Symfony\Component\DomCrawler\Crawler;
 
 class FacadeTest extends LaravelTestCase
 {
@@ -9,6 +11,8 @@ class FacadeTest extends LaravelTestCase
      |  Properties
      | ------------------------------------------------------------------------------------------------
      */
+    /** @var \Arcanedev\Breadcrumbs\Breadcrumbs */
+    private $breadcrumbs;
 
     /* ------------------------------------------------------------------------------------------------
      |  Main Functions
@@ -17,11 +21,19 @@ class FacadeTest extends LaravelTestCase
     public function setUp()
     {
         parent::setUp();
+
+        $this->breadcrumbs = Breadcrumbs::register('main', function($builder) {
+            /** @var Builder $builder */
+            $builder->push('Home',     'www.my-website.com');
+            $builder->push('About us', 'www.my-website.com/about-us');
+        });
     }
 
     public function tearDown()
     {
         parent::tearDown();
+
+        unset($this->breadcrumbs);
     }
 
     /* ------------------------------------------------------------------------------------------------
@@ -31,8 +43,32 @@ class FacadeTest extends LaravelTestCase
     /** @test */
     public function it_register_callbacks()
     {
-        Breadcrumbs::register('public', function($builder) {
+        $crawler = $this->getCrawler();
+        $this->assertEquals(1, $crawler->count());
+        $this->assertEquals(2, $crawler->children()->count());
+    }
 
-        });
+    /** @test */
+    public function it_can_generate_array()
+    {
+        $bc = $this->breadcrumbs->generateArray('main');
+
+        $this->assertEquals(2, count($bc));
+
+        $this->assertTrue($bc[0]->first);
+        $this->assertFalse($bc[0]->last);
+
+        $this->assertFalse($bc[1]->first);
+        $this->assertTrue($bc[1]->last);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Other Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function getCrawler()
+    {
+        return (new Crawler($this->breadcrumbs->render('main')))
+            ->filter('ul.breadcrumb.breadcrumb-top');
     }
 }
