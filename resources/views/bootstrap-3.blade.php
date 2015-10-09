c4a7aba735b0adedce7d5e65e9e3901dc8a2da28
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-09 19:51:09 +0100

    Updating views (Twitter Bootstrap)

--- resources/views/bootstrap-3.blade.php
+++ resources/views/bootstrap-3.blade.php
@@ -1,10 +1,10 @@
 @if ($breadcrumbs)
     <ul class="breadcrumb breadcrumb-top">
         @foreach ($breadcrumbs as $breadcrumb)
-            @if ($breadcrumb->url && ! $breadcrumb->isLast())
-                <li><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+            @if ($breadcrumb['url'] && ! $breadcrumb['last'])
+                <li><a href="{{ $breadcrumb['url'] }}">{{ $breadcrumb['title'] }}</a></li>
             @else
-                <li class="active">{{ $breadcrumb->title }}</li>
+                <li class="active">{{ $breadcrumb['title'] }}</li>
             @endif
         @endforeach
     </ul>
