7084068078b2678da2f410d3c25e0ea47437b2fa
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-21 16:13:10 +0000

    Updating the bootstrap v2 view

--- resources/views/bootstrap-2.blade.php
+++ resources/views/bootstrap-2.blade.php
@@ -1,17 +1,22 @@
 @if ($breadcrumbs)
     <ul class="breadcrumb">
         @foreach ($breadcrumbs as $breadcrumb)
+            <?php
+                $icon = isset($breadcrumb['icon'])
+                    ? '<i class="' . $breadcrumb['icon'] . '"></i> '
+                    : '';
+            ?>
             @if ($breadcrumb['last'])
-                <li class="active">{{ $breadcrumb['title'] }}</li>
+                <li class="active">{!! $icon !!}{{ $breadcrumb['title'] }}</li>
             @elseif ($breadcrumb->url)
                 <li>
-                    <a href="{{ $breadcrumb['url'] }}">{{ $breadcrumb['title'] }}</a>
+                    <a href="{{ $breadcrumb['url'] }}">{!! $icon !!}{{ $breadcrumb['title'] }}</a>
                     <span class="divider">/</span>
                 </li>
             @else
                 {{-- Using .active to give it the right colour (grey by default) --}}
                 <li class="active">
-                    {{ $breadcrumb['title'] }}
+                    {!! $icon !!}{{ $breadcrumb['title'] }}
                     <span class="divider">/</span>
                 </li>
             @endif
